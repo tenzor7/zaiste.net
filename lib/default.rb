e316fe5c0163f618eb807d83c12709d6b2199b2f
Zaiste! <oh@zaiste.net>
2012-05-19 17:31:57 +0200

    pygments.rb added, with erb prefiltering

--- lib/default.rb
+++ lib/default.rb
@@ -1,6 +1,8 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
 
+require 'pygments.rb'
+
 include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Blogging
 include Nanoc3::Helpers::Rendering
